7e80eb7c7ce88537f148ec8a0a5a6edd242ca839
Jordan Contreras <ljcontreras4@gmail.com>
2017-11-17 07:27:10 -0500

    first commit

---
+++ CoffeeShopApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CoffeeShopApp.Models;//don't forget to add
+
+namespace CoffeeShopApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Please fill in the form to register:";
+
+            return View();
+        }
+
+        public ActionResult DisplayName(SignUps NewSignUp)
+        {
+            ViewBag.Name = NewSignUp.FirstName;
+            return View();
+        }
+
+        
+
+    }
+}
---
+++ CoffeeShopApp/Models/SignUps.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoffeeShopApp.Models
+{
+    public class SignUps
+    {
+
+        private string firstname;
+        private string lastname;
+        private string email;
+        private string phone;
+        private string password;
+
+        public SignUps(): this("","","","","")
+        {
+            
+        }
+
+        public SignUps(string firstname, string lastname, string email, string phone, string password)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.email = email;
+            this.phone = phone;
+            this.password = password;
+        }
+
+       
+
+        public string FirstName
+        {
+            set { firstname = value; }
+            get { return firstname; }
+        }
+        public string LastName
+        {
+            set { lastname = value; }
+            get { return lastname; }
+        }
+
+        public string Email
+        {   
+            set { email = value; }
+            get { return email; }
+        }
+        public string Phone
+        {
+            set { phone = value; }
+            get { return phone; }
+        }
+       
+        public string Password
+        {
+            set { password = value; }
+            get { return password; }
+        }
+
+    }
+}
