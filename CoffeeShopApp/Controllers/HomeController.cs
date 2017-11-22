10746cdcc7d573143b8421bb007bb8e84e341480
Jordan Contreras <ljcontreras4@gmail.com>
2017-11-22 08:56:42 -0500

    added database

--- CoffeeShopApp/Controllers/HomeController.cs
+++ CoffeeShopApp/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 using CoffeeShopApp.Models;//don't forget to add
 
 namespace CoffeeShopApp.Controllers
@@ -23,6 +26,29 @@ namespace CoffeeShopApp.Controllers
 
             return View();
         }
+        public ActionResult SaveUser(User NewUserRecord)
+        {
+            //1.validate data
+
+            if (ModelState.IsValid)
+            {
+                //2.add the new record to the ORM, save changes to DB
+                CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+                ORM.Users.Add(NewUserRecord);
+                ORM.SaveChanges();
+
+                //3. showing the list of all the customers
+
+                return RedirectToAction("DisplayName");
+
+            }
+            else
+            {
+                //If validation fails
+                //go back to the form and show the list of errors
+                return View("NewCustomerForm");
+            }
+        }
 
         public ActionResult DisplayName(SignUps NewSignUp)
         {
@@ -30,6 +56,7 @@ namespace CoffeeShopApp.Controllers
             ViewBag.Name = NewSignUp.FirstName;
             return View();
         }
+
 
        
 
